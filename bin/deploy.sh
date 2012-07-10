1b6e517cbb5239896042700ff2debcf54f661a89
Tom Davidson <tom@davidson.me.uk>
2012-07-10 13:14:26 +0100

    Deploy now includes resources from MainBundle in preparation for design work

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,13 +5,20 @@ if [ ! -f ./src/NebulaFlow/bin/deploy.sh ]; then
 	exit
 fi
 
-echo "Copying config..."
+echo "Configuring framework"
 cp -f ./src/NebulaFlow/config/AppKernel.php ./app/
 cp -f ./src/NebulaFlow/config/routing.yml ./app/config/
 cp -f ./src/NebulaFlow/config/routing_dev.yml ./app/config/
 cp -f ./src/NebulaFlow/config/base.html.twig ./app/Resources/views/
-echo "NebulaFlow successfully updated it's config"
+echo "	Config updated"
+if [ -h ./web/bundles/nebulaflowmainbundle ]; then
+	echo "	OK (already linked)"
+else
+	echo "	Link established"
+	ln -s ../../src/NebulaFlow/MainBundle/Resources/public ./web/bundles/nebulaflowmainbundle
+fi
 
+echo ""
 echo -n "If you have set up your app/config/parameters.yml I can set up your database. Do you want me to install the database for you? [N]: "
 read -n 1 installDB
 echo ""
